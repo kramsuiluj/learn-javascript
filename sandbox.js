dc8ba52eacf5cfb0305e38632b4a96058cf15630
kramsuiluj <mjmaravillo@gmail.com>
2021-03-19 23:10:56 +0800

    basic variables

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+let age = 21;
+let year = 2021;
+
+console.log(age, year);
+
+age = 30;
+
+console.log(age);
+
+const points = 100;
+
+console.log(points);
+
+var score = 75;
+
+console.log(score);
