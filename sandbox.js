8838f98f4ba1d1a2496155199dbf8af6ce1b2d89
kramsuiluj <mjmaravillo@gmail.com>
2021-03-22 11:28:23 +0800

    array basics

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,31 @@
-// template strings
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30;
+// arrays
 
-// concatenation array
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
-// console.log(result);
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+// ninjas.push('test');
 
-// template string way
-result = `The blog called ${title} by ${author} has ${likes} likes.`;
+// console.log(ninjas);
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20, 25, 30, 35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+
+// console.log(random[3]);
+
+// console.log(ninjas.length);
+
+
+// Array Methods
+
+// let result = ninjas.join(', ');
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.concat(['ken', 'crystal']);
+let result = ninjas.push('ken');
+result = ninjas.pop(); // return the value of the popped array.
+
+
 console.log(result);
 
-// creating html templates
-let html = `
-    <h2>${title}</h2>
-    <p>By ${author}</p>
-    <span>This blog has ${likes} likes.</span>
-`;
-
-console.log(html);
