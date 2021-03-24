7478093207e4500e6b51f418257d6b17bc7c5fba
kramsuiluj <mjmaravillo@gmail.com>
2021-03-24 19:23:09 +0800

    difference of function and method

--- sandbox.js
+++ sandbox.js
@@ -1,51 +1,14 @@
-// regular function
-// const calcArea = function(radius){
-//     return 3.14 * radius**2;
-// }
+const name = 'Mark';
 
-// arrow function
-// There's no need to use parenthesis if there's only one parameter.
+// functions
 
-// const calcArea = (radius) => {
-//     return 3.14 * radius**2;
-// };
+const greet = () => 'hello';
 
-// Short version if there's only one parameter and one return value.
-// const calcArea = radius => 3.14 * radius**2;
+let resultOne = greet();
+console.log(resultOne);
 
 
+// methods
 
-// const area = calcArea(5);
-// console.log('Area is:', area);
-
-// Practice arrow functions
-
-// const greet = function(){
-//     return 'hello, world!';
-// }
-
-// const greet = () => 'hello, world!';
-// console.log(greet());
-
-// const bill = function(products, tax){
-//     let total = 0;
-//     for(let i = 0; i < products.length; i++){
-//         total += products[i] + products[i] * tax;
-//     }
-//     return total;
-// }
-
-const bill = (products, tax) => {
-
-    let total = 0;
-
-    for(let i = 0; i < products.length; i++){
-        total += products[i] + products[i] * tax;
-    }
-
-    return total;
-
-};
-
-const total = bill([100, 200, 50], 0.2);
-console.log(total);
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
