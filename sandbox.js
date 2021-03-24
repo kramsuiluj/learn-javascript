579ba6ee2f98b09d02fe3a7b16eaad7429f96945
kramsuiluj <mjmaravillo@gmail.com>
2021-03-24 21:54:22 +0800

    object literal notation pt.2

--- sandbox.js
+++ sandbox.js
@@ -6,20 +6,47 @@ let user = {
     age: 21,
     email: 'diana@gmail.com',
     location: 'Philippines',
-    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+
+    login: () => {
+        console.log('The user logged in');
+    },
+
+    logout: () => {
+        console.log('The user logged out');
+    },
+
+    // my correct solution
+    // logBlogs: blogs => {
+    //     blogs.forEach(blog => {
+    //         console.log(blog);
+    //     });
+    // }
+
+    // logBlogs: () => {
+    //     this.blogs.forEach(blog => {
+    //         console.log(blog);
+    //     });
+    // }
+
+    logBlogs: function(){
+        console.log('This user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
+    }
+
+    // logBlogs: () => {
+    //     console.log('This user has written the following blogs:');
+    //     this.blogs.forEach(blog => {
+    //         console.log(blog);
+    //     });
+    // }
 
 };
 
-// console.log(user);
-// console.log(user.name);
-
-// user.age = 22;
-// console.log(user.age);
-
-const key = 'location';
-
-console.log(user[key]);
-user['name'] = 'gazelle';
-console.log(user['name']);
-
-console.log(typeof user);
+// user.login();
+// user.logout();
+// user.logBlogs(user.blogs); // my correct solution
+// user.logBlogs();
+user.logBlogs();
