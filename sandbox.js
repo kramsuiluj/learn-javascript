fa1e41ec5ad96a93eb2c090c64c9bc99504adf6c
kramsuiluj <mjmaravillo@gmail.com>
2021-03-25 11:58:47 +0800

    DOM get element/s by

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,14 @@
-// document object model
+// get element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
 
-// const para = document.querySelector('p'); // Getting the first p tag in the heirarchy.
-// const para = document.querySelector('.error'); // Getting any element with the class of '.error'
-// const para = document.querySelector('div.error'); // Getting the specific element with the class of '.error'
 
-/* Note:Getting the unique selector of an element.
+// get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors[1]);
 
-1. Right click the HTML element in the browser then click inspect.
-2. Right click the highlighted line of code then click copy and choose copy selector.
 
-*/
-
-// console.log(para);
-
-// const paras = document.querySelectorAll('p');
-const errors = document.querySelectorAll('.error');
-
-// paras.forEach(para => {
-
-//     console.log(para);
-
-// });
-
-// console.log(paras[0]);
-
-// console.log(errors);
-
-errors.forEach(error => {
-    console.log(error);
-});
+// get element by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
