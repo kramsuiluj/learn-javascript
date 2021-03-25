61c0ddc39d39440a6bddc9a7a45da4c3d6f69137
kramsuiluj <mjmaravillo@gmail.com>
2021-03-25 14:35:09 +0800

    adding and removing classes on an element

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,46 @@
-const title = document.querySelector('h1');
-// title.setAttribute('style', 'margin: 50px;');
+// Adding and removing classes on an element.
 
-console.log(title.style);
-console.log(title.style.color);
+// const content = document.querySelector('p');
+// console.log(content.classList);
+// content.classList.add('error');
+// content.classList.remove('error');
+// content.classList.add('success');
 
-title.style.margin = '50px';
-title.style.color = 'crimson';
-title.style.fontSize = '50px';
-title.style.margin = '';
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     if (para.innerText.includes('error')) {
+//         para.classList.add('error');
+//     } else if (para.innerText.includes('success')) {
+//         para.classList.add('success');
+//     } else {
+//         para.classList.add('default');
+//     }
+// });
+
+paras.forEach(para => {
+
+    if (para.textContent.includes('error')) {
+
+        para.classList.add('error');
+
+    } else if (para.textContent.includes('success')) {
+
+        para.classList.add('success');
+
+    } else {
+        
+        para.classList.add('default');
+
+    }
+
+    console.log(para.textContent);
+
+});
+
+
+
+const title = document.querySelector('.title');
+
+title.classList.toggle('test');
+title.classList.toggle('test');
